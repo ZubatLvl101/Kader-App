3d9e4ed6375f54a78374080d64787a29df0e9d0e
Julian <julian.zubert@fh-bielefeld.de>
2022-03-21 15:55:25 +0100

    Css läuft jetzt

--- kader/src/routes/Tabelle.js
+++ kader/src/routes/Tabelle.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import { Container } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 //import store from '../store';
 import { Navigate } from 'react-router-dom';
 import styles from '../styles/routes/Home.module.css';
@@ -18,7 +19,24 @@ const Tabelle = () => {
                 <div>
                     <div className={styles.text}>
                         <h3 className={styles.title}>Tabelle</h3>
-                        Hello World
+                        <div >
+                            <Table striped bordered hover variant="dark">
+                                <thead>
+                                    <tr>
+                                        <th>Position</th>
+                                        <th>Name</th>
+                                        <th>Punkte</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Test</td>
+                                        <td>Test</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </div>
                     </div>
                 </div>                
             </Container>
